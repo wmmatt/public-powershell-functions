d253131d926ffe8623a26693929fd65f0491f3cd
wmmatt <87339922+wmmatt@users.noreply.github.com>
2023-12-27 16:27:31 -0600

    Added comments

--- Confirm-RegistryValue.ps1
+++ Confirm-RegistryValue.ps1
@@ -1,4 +1,19 @@
 function Confirm-RegistryValue {
+    <#
+        .DESCRIPTION
+        Checks for a specific value of the key/name you specify and returns $true if the value matches, or $false if the value does not match.
+
+        .EXAMPLE
+        PS> Confirm-RegistryValue -Path 'HKLM:\\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest' -Name 'UseLogonCredential' -Value 0 -Method test
+        True
+
+        PS> Confirm-RegistryValue -Path 'HKLM:\\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest' -Name 'UseLogonCredential' -Value 1 -Method test
+        False
+
+        PS> Confirm-RegistryValue -Path 'HKLM:\\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest' -Name 'UseLogonCredential' -Value 1 -Method set
+        True
+    #>
+    
     param (
         [string]$Path,
         [string]$Name,
