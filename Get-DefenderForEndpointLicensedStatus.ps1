051783ab9e6f925280e06b9f980295a546ae9882
Matthew Weir <87339922+wmmatt@users.noreply.github.com>
2024-10-29 09:21:25 -0500

    Create Get-DefenderForEndpointLicensedStatus.ps1

---
+++ Get-DefenderForEndpointLicensedStatus.ps1
@@ -0,0 +1,15 @@
+Get-DefenderForEndpointLicensedStatus {
+    # Define the registry path and property name
+    $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows Advanced Threat Protection\Status"
+    $propertyName = "OnboardingState"
+
+    # Check if the registry key and property exist and retrieve the value
+    $onboardingState = Get-ItemProperty -Path $registryPath -Name $propertyName -EA 0
+
+    # Return $true if the value is 1, otherwise return $false
+    if ($onboardingState.$propertyName -eq 1) {
+        return $true
+    } else {
+        return $false
+    }
+}
